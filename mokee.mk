9055b406056fe12a93ce9c4a7a81d50df6e5fbcb
martincz <martincz@mokeetek.com>
2013-11-17 19:01:11 +0800

    mokee modify
    Change-Id: Ie8f539987964c2576fb61b5b16e864817c2e77c1

--- mokee.mk
+++ mokee.mk
@@ -1,17 +1,17 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/mk/config/gsm.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/p930/p930.mk)
 
-PRODUCT_NAME := cm_p930
+PRODUCT_NAME := mk_p930
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := OptimusLTE
